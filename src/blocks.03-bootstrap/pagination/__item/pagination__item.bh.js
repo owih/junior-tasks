5d46ca43f080f2939a9a5ac7bca61629f44b9261
zvezdilin <artem@intervolga.ru>
2018-11-21 15:59:06 +0400

    pagination

--- src/blocks.03-bootstrap/pagination/__item/pagination__item.bh.js
+++ src/blocks.03-bootstrap/pagination/__item/pagination__item.bh.js
@@ -2,14 +2,16 @@ module.exports = function (bh) {
     bh.match('pagination__item', function (ctx, json) {
         ctx.tag('li');
         ctx.mix({block: 'page-item'});
-        let active = json.active;
+        let props = Object.assign({
+            active: false,
+        }, json.props ? json.props : {});
         let content = ctx.content();
         if (ctx.isSimple(content)) {
-            if (!active) {
-                ctx.content({block: 'a', cls: 'page-link', content: content}, true);
-            } else {
+            if (props.active) {
                 ctx.mix({block: 'active'});
                 ctx.content({tag: 'span', cls: 'page-link', content: content}, true);
+            } else {
+                ctx.content({block: 'a', cls: 'page-link', content: content}, true);
             }
         }
     });
