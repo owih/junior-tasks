16999d5058aa67dd37bc19b556ffa1c3927ab219
Zvezdilin <artem@intervolga.ru>
2019-05-20 19:07:21 +0400

    addon viewport

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,1 +1,3 @@
 import '@storybook/addon-notes/register';
+import '@storybook/addon-viewport/register';
+import '@storybook/addon-actions/register';
--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,9 @@ import './../dist/merged.css';
 import './../dist/assets/merged.js';
 
 import pretty from  'pretty';
+import {document, console} from 'global';
 
+import { withActions } from '@storybook/addon-actions';
 import { configure, storiesOf, addParameters} from '@storybook/html';
 
 const tmpls = require.context('./../src', true, /tmpl-specs.*\.bemjson\.js$/);
@@ -32,11 +34,7 @@ configure(() => {
       const variant = match[2];
       const bemjson = JSON.stringify(json, null, 2);
       const html = pretty(bh.apply(json), {ocd: true});
-      const docs = {
-        name: name,
-        bemjson: bemjson,
-        html: html,
-      };
+      const docs = { name: name, bemjson: bemjson, html: html };
       const notes = getNotes(docs);
       storiesOf(`Components | ${name}`, module)
         .add(variant, () => html, {
