418cd30747abc6b667f8fa45bb4a588130534dd4
ZvezdilinAK <artem@intervolga.ru>
2022-04-12 12:31:21 +0300

    [bootstrap5] Упрощены зависимости. Обновлен bootstrap

--- src/blocks.02-common/form-control/form-control.stories.js
+++
@@ -1,6 +0,0 @@
-const {getExample} = require('helpers');
-module.exports = Object.assign({
-  default: {
-    title: 'Blocks/form-control',
-  },
-}, getExample(require.context('!!babel-loader!./', true, /\.tmpl-specs\/(.*).bemjson\.js$/)));
